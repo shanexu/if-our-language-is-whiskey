f24cee4159695af7d4669a3e0bd5f2eb8a1a4169
Xu Shane <xusheng0711@gmail.com>
2020-09-23 10:38:56 +0800

    modify code

--- src/main/scala/org/xusheng/ioliw/sample.scala
+++ src/main/scala/org/xusheng/ioliw/sample.scala
@@ -13,13 +13,6 @@ object sample {
 
   case class Dep(name: NodeName, deps: List[NodeName])
 
-  def latency[F[_] : Concurrent](msg: String): F[Unit] =
-    for {
-      _ <- Sync[F].delay(println(s"--> [${Thread.currentThread.getId}] $msg"))
-      _ <- Sync[F].delay(Thread.sleep(1000))
-      _ <- Sync[F].delay(println(s"<-- [${Thread.currentThread.getId}] $msg"))
-    } yield ()
-
   val nodeDatabase: Map[NodeName, Node] = Map(
     "A" -> Node("A"),
     "B" -> Node("B"),
@@ -32,27 +25,37 @@ object sample {
   object Nodes extends Data[NodeName, Node] {
     def name = "Nodes"
 
-    def source[F[_] : ConcurrentEffect]: DataSource[F, NodeName, Node] = new DataSource[F, NodeName, Node] {
-      override def data = Nodes
+    def source[F[_] : ConcurrentEffect]: DataSource[F, NodeName, Node] =
+      new DataSource[F, NodeName, Node] {
+        override def data = Nodes
 
-      override def CF = ConcurrentEffect[F]
+        override def CF = ConcurrentEffect[F]
 
-      override def fetch(id: NodeName): F[Option[Node]] =
-        latency[F](s"One Node $id") >> CF.pure(nodeDatabase.get(id))
+        override def fetch(id: NodeName): F[Option[Node]] =
+          latency[F](s"One Node $id") >> CF.pure(nodeDatabase.get(id))
 
-      // override def maxBatchSize: Option[Int] = Some(2)
+        // override def maxBatchSize: Option[Int] = Some(2)
 
-      override def batchExecution: BatchExecution = InParallel
+        override def batchExecution: BatchExecution = InParallel
 
-      override def batch(ids: NonEmptyList[NodeName]): F[Map[NodeName, Node]] =
-        latency[F](s"Batch Nodes $ids") >> CF.pure(nodeDatabase.filterKeys(ids.toList.toSet))
-    }
+        override def batch(ids: NonEmptyList[NodeName]): F[Map[NodeName, Node]] =
+          latency[F](s"Batch Nodes $ids") >>
+            CF.pure(nodeDatabase.filterKeys(ids.toList.toSet))
+      }
   }
+
+  def latency[F[_] : Concurrent](msg: String): F[Unit] =
+    for {
+      _ <- Sync[F].delay(println(s"--> [${Thread.currentThread.getId}] $msg"))
+      _ <- Sync[F].delay(Thread.sleep(1000))
+      _ <- Sync[F].delay(println(s"<-- [${Thread.currentThread.getId}] $msg"))
+    } yield ()
 
   def getNode[F[_] : ConcurrentEffect](id: NodeName): Fetch[F, Node] =
     Fetch(id, Nodes.source)
 
-  def getGraph[F[_] : ConcurrentEffect](id: NodeName, deps: Map[NodeName, List[NodeName]]): Fetch[F, Node] =
+  def getGraph[F[_] : ConcurrentEffect]
+  (id: NodeName, deps: Map[NodeName, List[NodeName]]): Fetch[F, Node] =
     for {
       _ <- deps.get(id).map {
         ids => ids.traverse(i => getGraph(i, deps))
@@ -66,12 +69,12 @@ object sample {
     import scala.concurrent.ExecutionContext
     import scala.concurrent.duration._
 
-    val executor = new ScheduledThreadPoolExecutor(4)
-    val executionContext: ExecutionContext = ExecutionContext.fromExecutor(executor)
+    val executor = new ScheduledThreadPoolExecutor(1)
+    val executionContext: ExecutionContext =
+      ExecutionContext.fromExecutor(executor)
 
     implicit val timer: Timer[IO] = IO.timer(executionContext)
     implicit val cs: ContextShift[IO] = IO.contextShift(executionContext)
-
 
     val deps = Map(
       "A" -> List("B", "C"),
